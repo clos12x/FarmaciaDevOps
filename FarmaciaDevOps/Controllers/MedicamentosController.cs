7ac8d26d06f25376f3ccdf365c0cb695c2e8655e
clos12x <joseluistaguaroca22@gmail.com>
2026-05-21 17:46:46 -0400

    Initial commit

---
+++ FarmaciaDevOps/Controllers/MedicamentosController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FarmaciaDevOps.Models;
+
+namespace FarmaciaDevOps.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MedicamentosController : ControllerBase
+    {
+        static List<Medicamento> medicamentos = new List<Medicamento>()
+        {
+            new Medicamento
+            {
+                Id = 1,
+                Nombre = "Paracetamol",
+                Precio = 5,
+                Stock = 20
+            },
+
+            new Medicamento
+            {
+                Id = 2,
+                Nombre = "Ibuprofeno",
+                Precio = 8,
+                Stock = 15
+            }
+        };
+
+        // GET: api/medicamentos
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(medicamentos);
+        }
+
+        // POST: api/medicamentos
+        [HttpPost]
+        public IActionResult Post(Medicamento medicamento)
+        {
+            medicamentos.Add(medicamento);
+
+            return Ok(medicamento);
+        }
+    }
+}
---
+++ FarmaciaDevOps/Models/Medicamento.cs
@@ -0,0 +1,13 @@
+﻿namespace FarmaciaDevOps.Models
+{
+    public class Medicamento
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
